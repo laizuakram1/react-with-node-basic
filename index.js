58ad08fae4e666e4d86c1c38e7ce4bdca0de65a1
laizuakram1 <laizuakram98@gmail.com>
2022-04-23 23:43:29 +0600

    node connected with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(bodyParser.json())
+
+// mongodb connect code
+
+const uri = "mongodb+srv://userDb1:AA4f9hFI6MZC6QCd@cluster0.mq9ya.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('db connected');
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 
 
 const users = [
